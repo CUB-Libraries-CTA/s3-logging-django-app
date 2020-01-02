535b19b34aa92e90405ab53212f68d37e8494345
dutr5288 <dutr5288@colorado.edu>
2020-01-02 16:40:50 -0700

    Added: Cronjob

--- cronjob-upload-to-s3.py
+++ cronjob-upload-to-s3.py
@@ -7,15 +7,15 @@ import logging
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-def upload_to_s3_job():
-    now = datetime.now()
-    s3_client = boto3.client('s3')
-    current_date = now.strftime("%Y-%m-%d")
-    filePath = os.path.join(BASE_DIR, 'logs/room-booking/log-' + current_date + '.csv')
-    try:
-        response = s3_client.upload_file(
-            filePath, 'cubl-log', "room-booking/" + current_date + '.csv')
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return response
+
+now = datetime.now()
+s3_client = boto3.client('s3')
+current_date = now.strftime("%Y-%m-%d")
+filePath = os.path.join(
+    BASE_DIR, 'logs/room-booking/log-' + current_date + '.csv')
+try:
+    response = s3_client.upload_file(
+        filePath, 'cubl-log', "room-booking/" + current_date + '.csv')
+    os.remove(filePath)
+except ClientError as e:
+    logging.error(e)

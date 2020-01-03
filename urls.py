de4d238aba4b90f3cf4b9feb0f20591885e31969
dutr5288 <dutr5288@colorado.edu>
2020-01-03 14:58:54 -0700

    Added : uploadview

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import LogView
+from .views import LogView,UploadView
 
 urlpatterns = [
     path('log', LogView.as_view(), name='log'),
+    path('upload', UploadView.as_view(), namg='upload')
 ]
--- views.py
+++ views.py
@@ -5,9 +5,12 @@ from rest_framework.permissions import IsAuthenticated
 import json
 import requests
 from .permission import IsAdmin
-from datetime import datetime
-import logging
+import boto3
+from botocore.exceptions import ClientError
 import os
+from datetime import datetime, timedelta
+import logging
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -26,3 +29,25 @@ class LogView(APIView):
         f.write(message + "\n")
         f.close()
         return Response({'message': 'success'}, status=status.HTTP_200_OK)
+
+class UploadView(APIView):
+    http_metthod_names = ['post']
+
+    def post(self, request):
+        s3_client = boto3.client('s3')
+        yesterday_date = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
+        filePath = os.path.join(
+            BASE_DIR, 'logs/room-booking/log-' + yesterday_date + '.csv')
+        if os.path.isfile(filePath):
+            try:
+                response = s3_client.upload_file(
+                    filePath, 'cubl-log', "room-booking/" + yesterday_date + '.csv')
+                os.remove(filePath)
+            except ClientError as e:
+                logging.error(e)
+            return Response({'message': 'success'}, status=status.HTTP_200_OK)
+        else :
+            return Response({'message': 'file is not exits'}, status=status.HTTP_200_OK)
+
+
+
